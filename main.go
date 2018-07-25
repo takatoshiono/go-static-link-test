8ec81f02938a259971e0e2c24cf19dbc766947a4
Takatoshi Ono <takatoshi.ono@gmail.com>
2018-07-25 17:57:57 +0900

    this is error
    Error:
    standard_init_linux.go:190: exec user process caused "no such file or directory"

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"os"
+)
 
 func main() {
 	fmt.Println("hello world.")
+
+	addrs, err := net.LookupHost("golang.org")
+	if err != nil {
+		fmt.Printf("failed to lookup: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("addresses of golang.org: %v\n", addrs)
 }
